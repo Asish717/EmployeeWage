43e590b83fd9828006ccbe0dbce63f8d6f634a92
Asish <asishkumar717@gmail.com>
2023-11-30 12:23:53 +0530

    Added employee daily wage

--- src/main/java/com/java/EmpWageBuilder.java
+++ src/main/java/com/java/EmpWageBuilder.java
@@ -2,13 +2,20 @@ package com.java;
 
 public class EmpWageBuilder {
     public static void main(String[] args) {
+        // Constants
         int IS_FULL_TIME=1;
+        int EMP_RATE_PER_HOUR=20;
+        // Variables
+        int empHrs=0,empWage=0;
+        //Computation
         double empCheck=Math.floor(Math.random()*10)%2;
         if (empCheck==IS_FULL_TIME){
-            System.out.println("Employee is Present");
+            empHrs=8;
         }
         else {
-            System.out.println("Employee is Absent");
+            empHrs=0;
+            empWage=empHrs*EMP_RATE_PER_HOUR;
+            System.out.println("Emp wage: "+empWage);
         }
     }
 }
